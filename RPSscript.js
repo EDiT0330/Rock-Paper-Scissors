958dcd2bffd8bc11a4fac375b57355790d7a5bb6
Ethan <ethan.tu246@gmail.com>
2021-07-08 11:48:57 -0700

    included score as well as who won or loss

--- RPSscript.js
+++ RPSscript.js
@@ -1,10 +1,10 @@
 //let playerSelection;
 //let computerSelection;
-let playerScore;
-let computerScore;
+
+let playerScore = 0;
+let computerScore = 0; 
 
 //Function lets the computer make a choice between rock, paper or scissors
-
 function computerPlay(){
     let pickItem = Math.floor(Math.random()*3);
 
@@ -19,7 +19,6 @@ function computerPlay(){
 }
 
 //Function plays one round of RPS with the players input as well as the random selection created by the computer
-
 function playRound(playerSelection, computerSelection){
 
     console.log(playerSelection);
@@ -29,36 +28,61 @@ function playRound(playerSelection, computerSelection){
     
     if(playerSelection == computerSelection.toLowerCase()){
         return "Tie";
-    } else if(playerSelection == "Rock".toLowerCase()){
+    } else if(playerSelection == "rock"){
         if(computerSelection == "Scissors"){
+            playerScore++;
             return "You win, Rock beats Scissors";
         } else if(computerSelection == "Paper"){
-            return "You lose, paper beats rock"
+            computerScore++;
+            return "You lose, paper beats rock"; 
         }
-    } else if(playerSelection == "Paper".toLowerCase()){
+    } else if(playerSelection == "paper"){
         if(computerSelection == "Rock"){
+            playerScore++;
             return "You win, Paper beats Rock";
         } else if(computerSelection == "Scissors"){
+            computerScore++;
             return "You lose, Scissors beats Paper";
         }      
-    } else if(playerSelection == "Scissors".toLowerCase()){
+    } else if(playerSelection == "scissors"){
         if(computerSelection == "Paper"){
+            playerScore++;
             return "You win, Scissors beats Paper";
         } else if(computerSelection == "Rock"){
+            computerScore++;
             return "You lose, Rock beats Scissors";
         }
     }
+
 }
 
-/*
+//Function plays the game
 function game(){
-    for(var i=0; i<5; i++){
-        playRound(playerSelection, computerSelection);
+
+    const playerSelection = window.prompt("Your Move");
+    const computerSelection = computerPlay();
+    
+    
+    console.log(playRound(playerSelection, computerSelection))
+    
+    if(playerScore == computerScore){
+        return "Nothing Happens";
     }
+        else if(playerScore>computerScore){
+            return "You Win";
+        }
+           else{
+            return "Computer Wins";
+           } 
 }
-*/
 
-const playerSelection = window.prompt("Your Move");
-const computerSelection = computerPlay();
 
-console.log(playRound(playerSelection, computerSelection));
+//const playerSelection = window.prompt("Your Move");
+//const computerSelection = computerPlay();
+
+//console.log(playRound(playerSelection, computerSelection));
+
+console.log(game());
+
+console.log("Your score: " + playerScore);
+console.log("Computer score: " + computerScore);
